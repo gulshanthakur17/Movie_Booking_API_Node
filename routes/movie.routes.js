c6adc2afb14a8cd4531598b5e26e037cf492efcd
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-05 21:48:15 +0530

    Feature: Added Delete Movie & Get Movie Api with code segregation into servies

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,9 +1,25 @@
-const MovieController = require('../controllers/movie.controller');
+const movieController = require('../controllers/movie.controller');
+//const movieMiddlewares = require('../middlewares/movie.middlewares');
 
 
 const routes = (app) => {
     // routes function takes express app object as parameter
-    app.post('/mba/api/v1/movies', MovieController.createMovie);
+    app.post(
+        '/mba/api/v1/movies', 
+        //movieMiddlewares.validateMovieCreateRequest,
+        movieController.createMovie
+        );
+
+    app.delete(
+        '/mba/api/v1/movies/:id',
+        movieController.deleteMovie
+    )
+
+    app.get(
+        '/mba/api/v1/movies/:id',
+        movieController.getMovie
+    )
 }
+
 
 module.exports = routes;
