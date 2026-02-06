351cf0e6048c62d682245bfcd515950eba5a8fa1
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-06 16:32:09 +0530

    Feature: Added validate middleware

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,9 +1,11 @@
 const theatreController = require('../controllers/theatre.controllers');
+const theatreMiddleware = require('../middlewares/theatre.middlewares');
 
 
 const routes = (app) => {
     app.post(
         '/mba/api/v1/theatre',
+        theatreMiddleware.validateTheatreCreateRequest,
         theatreController.create
     );
     
