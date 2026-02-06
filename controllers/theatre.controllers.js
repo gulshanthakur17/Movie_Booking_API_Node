1884f013fbe2eb44d318148ee1579a78f8bb99e1
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-06 20:46:47 +0530

    Feature: Added update api

--- controllers/theatre.controllers.js
+++ controllers/theatre.controllers.js
@@ -63,10 +63,28 @@ const getAllTheatre = async (req , res) => {
     }
 }
 
+const update = async (req, res) => {
+    try {
+        const response = await theatreService.updateTheatre(req.params.id, req.body);
+        if(response.err){
+            errorResponseBody.err = response.err;
+            return res.status(response.code).json(errorResponseBody);
+        }
+        successResponseBody.data = response;
+        successResponseBody.message = "Successfully updated the theatre";
+        return res.status(200).json(successResponseBody);
+    } catch (error) {
+        console.log(error);
+        errorResponseBody.err = error;
+        return res.status(500).json(errorResponseBody);
+    }
+}
+
 
 module.exports = {
     create,
     destroy,
     getTheatre,
     getAllTheatre,
+    update
 }
