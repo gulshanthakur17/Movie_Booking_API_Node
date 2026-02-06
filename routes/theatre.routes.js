fa482019dc708b8c8408a0669c18fa4805a069d1
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-06 20:08:24 +0530

    Feature: Added Update api & Comment

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -3,25 +3,43 @@ const theatreMiddleware = require('../middlewares/theatre.middlewares');
 
 
 const routes = (app) => {
+    // routes function takes express app object as parameters
+
+    //CREATE
     app.post(
         '/mba/api/v1/theatre',
         theatreMiddleware.validateTheatreCreateRequest,
         theatreController.create
     );
 
+    // DELETE
     app.delete(
         '/mba/api/v1/theatre/:id',
         theatreController.destroy
     );
 
+    //READ
     app.get(
         '/mba/api/v1/theatre/:id',
         theatreController.getTheatre
     );
 
+    //READ
     app.get(
         '/mba/api/v1/theatre',
         theatreController.getAllTheatre
+    );
+
+    //UPDATE
+    app.patch(
+        '/mba/api/v1/theatre/:id',
+        theatreController.update
+    );
+
+    //UPDATE
+    app.put(
+        '/mba/api/v1/theatre/:id',
+        theatreController.update
     );
     
 }
