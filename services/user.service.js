8549465abf415080a0051aed86f37de069c8b331
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-08 18:07:25 +0530

    Feature: Added isauthenticated middleware for checking api auth

--- services/user.service.js
+++ services/user.service.js
@@ -46,8 +46,22 @@ const getUserByEmail = async (email) => {
     }
 }
 
+const getUserById = async (id) => {
+    try {
+        const user = await User.findById(id);
+        if(!user) {
+            throw {err: 'No user found for the given id', code: 404};
+        }
+        return user;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
 
 module.exports = {
     createUser,
     getUserByEmail,
+    getUserById,
 }
