9a4bc188bf7e5559ddc57b192e6c11692ec8cc66
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-08 14:16:29 +0530

    Feature: Added input password comarison logic

--- models/user.model.js
+++ models/user.model.js
@@ -46,6 +46,18 @@ userSchema.pre('save', async function () {
     this.password = hash;
 });
 
+/**
+ * This is going to be an instance method for user, to compare a password with the stored encrypted password
+ * @param  plainPassword -> input password given by user in sign in request
+ * @returns boolean denoting whether passwords are same or not ?
+ */
+
+userSchema.methods.isValidPassword = async (plainPassword) => {
+    const currentUser = this;
+    const compare = await bcrypt.compare(plainPassword, currentUser.password);
+    return compare;
+}
+
 
 const User = mongoose.model('User', userSchema);
 
