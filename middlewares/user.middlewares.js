0395f5876dc0e4debbf6f375b3ad31d1b86f7307
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-10 12:46:40 +0530

    Refactor: Replace hard-coded status codes

--- middlewares/user.middlewares.js
+++ middlewares/user.middlewares.js
@@ -1,10 +1,11 @@
 const { errorResponseBody } = require('../utils/responsebody');
+const  { STATUS_CODES } = require('../utils/constants');
 
 const validateUpdateUserRequest = (req, res, next) => {
     // validate presence of atleast one of the two i.e. userRole or userStatus
     if(!(req.body.userRole || req.body.userStatus)) {
         errorResponseBody.err = 'Malformed request, please send atleast one parameter';
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
     next();
 
