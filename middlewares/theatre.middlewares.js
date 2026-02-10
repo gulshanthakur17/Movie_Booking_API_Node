6b981b43dc5703d3bf8932b2adaae6a53876b66a
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-10 12:41:42 +0530

    Fix: Added codes for middlewares of theatre api

--- middlewares/theatre.middlewares.js
+++ middlewares/theatre.middlewares.js
@@ -1,4 +1,5 @@
 const {errorResponseBody } = require('../utils/responsebody');
+const { STATUS_CODES } = require('../utils/constants');
 
 
 /**
@@ -15,19 +16,19 @@ const validateTheatreCreateRequest = async (req, res, next) => {
     //validate the presence of name
     if(!req.body.name){
         errorResponseBody.message = "The name of the theatre is not present in the request";
-        return res.status(400).json(errorResponseBody)
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody)
     }
 
     //validate for the presence of pincode
     if(!req.body.pincode){
         errorResponseBody.message = "The pincode of the theatre is not present in the request";
-        return res.status(400).json(errorResponseBody)
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody)
     }
 
     //validate for the presence of city
     if(!req.body.city){
         errorResponseBody.message = "The city of the theatre is not present in the request";
-        return res.status(400).json(errorResponseBody)
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody)
     }
 
     next();// everything is fine move to the next middleware
@@ -37,22 +38,22 @@ const validateUpdateMoviesRequest = async (req, res, next) => {
     //validation of insert parameter
     if(req.body.insert == undefined) {
         errorResponseBody.message = "The insert parameter is missing in the request";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
     // validate moviesIds presence
     if(!req.body.movieIds) {
         errorResponseBody.message = "No movies present in the request to be updated in theater";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
     // validate if moviesIds is an array or not
     if(!(req.body.movieIds instanceof Array)) {
         errorResponseBody.message = "Expected array of movies but found something else";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
     // validate if moviesIds is empty or not
     if(req.body.movieIds.length == 0) {
         errorResponseBody.message = "No movies present in the array provided";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
     // everything is fine
     next();
